81d78ae79410f91056d86494f77db51b83ed09cb
Robin Zimmer <robinzimmer1989@web.de>
2020-11-04 17:39:41 -0800

    return featured image

--- utils/format_post.php
+++ utils/format_post.php
@@ -10,7 +10,10 @@ function gcms_format_post($site_id, $post) {
     'postTypeID' => $post->post_type,
     'parentID' => $post->post_parent,
     'status' => $post->post_status,
-    'featuredImage' => null,
+    'featuredImage' => [
+      'id' => get_post_thumbnail_id($post->ID),
+      'url' => get_the_post_thumbnail_url($post->ID)
+    ],
     'content' => [],
     'template' => [],
     'createdAt' => $post->post_date,
