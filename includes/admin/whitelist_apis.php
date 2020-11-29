712328fa4dde03cb9e26b5db7f3f6a4b7d4d98d4
Robin Zimmer <robinzimmer1989@web.de>
2020-11-28 18:20:38 -0800

    fix api whitelist for multisite

--- includes/admin/whitelist_apis.php
+++ includes/admin/whitelist_apis.php
@@ -1,10 +1,20 @@
 <?php
 
 add_filter( 'jwt_auth_whitelist', function ( $endpoints ) {
-  return array(
+
+  if(is_multisite()){
+    $site = get_blog_details();
+
+    return array(
+      $site->path . 'wp-json/jamcms/v1/test',
+      $site->path . 'wp-json/jamcms/v1/getBuildSite',
+    );
+  }else{
+    return array(
       '/wp-json/jamcms/v1/test',
       '/wp-json/jamcms/v1/getBuildSite',
-  );
+    );
+  }
 });
 
 ?>
