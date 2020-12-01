fef5b3ddd1bc52208f13b6931b1e8f85f3f28fd6
Robin Zimmer <robinzimmer1989@web.de>
2020-11-30 20:57:49 -0800

    format and return url for menu item

--- includes/utils/format_menu_item.php
+++ includes/utils/format_menu_item.php
@@ -7,7 +7,7 @@ function jam_cms_format_menu_item($menu_item){
     'postID'      => $menu_item->object == 'custom' ? null : (int) $menu_item->object_id,
     'postTypeID'  => $menu_item->object == 'custom' ? null : $menu_item->object,
     'title'       => $menu_item->title,
-    'url'         => $menu_item->object == 'custom' ? $menu_item->url : null,
+    'url'         => jam_cms_format_url($menu_item->url),
     'children'    => $menu_item->children ? $menu_item->children : [],
   ];
 
