9285da300c2f7a318c658463d550af4b9e2b5a1d
Robin Zimmer <robinzimmer1989@web.de>
2020-12-11 18:49:00 -0800

    lower user role for site fetch

--- includes/api/site/get_site.php
+++ includes/api/site/get_site.php
@@ -6,7 +6,7 @@ function jam_cms_api_get_site() {
         'methods' => 'POST',
         'callback' => 'jam_cms_api_get_site_callback',
         'permission_callback' => function () {
-            return current_user_can( 'edit_posts' );
+            return current_user_can( 'read' );
         }
     ));
 }
