32e7cda118ae6b6c2385241ef0af0726c9946de7
Robin Zimmer <robinzimmer1989@web.de>
2020-12-12 16:01:49 -0800

    add additional checks for menu

--- includes/queries/get_menu_by_id.php
+++ includes/queries/get_menu_by_id.php
@@ -1,7 +1,17 @@
 <?php
 
 function jam_cms_get_menu_by_id($menu_id, $format = true){
+
+  if(!$menu_id){
+    return false;
+  }
+
   $menu_items = wp_get_nav_menu_items($menu_id);
+
+  if(!$menu_items){
+    return false;
+  }
+
   return jam_cms_build_menu_tree($menu_items);
 }
 
