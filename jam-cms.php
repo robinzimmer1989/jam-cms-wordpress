2134181ccfa341fb1abc67dc439a892b9800d44a
Robin Zimmer <robinzimmer1989@web.de>
2020-12-31 09:48:11 -0500

    version 0.1.1

--- jam-cms.php
+++ jam-cms.php
@@ -8,7 +8,7 @@
  * Plugin Name:       jamCMS
  * Plugin URI:        https://github.com/robinzimmer1989/jam-cms-wordpress
  * Description:       
- * Version:           0.1.0
+ * Version:           0.1.1
  * Author:            Robin Zimmer
  * Author URI:        https://github.com/robinzimmer1989
  * License:           
