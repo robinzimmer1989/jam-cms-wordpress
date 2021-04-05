210a5902aaf8d8e98a65904ea3727e1a1c37c188
Robin Zimmer <robinzimmer1989@web.de>
2021-04-05 16:57:45 -0700

    add uri

--- includes/utils/format_term.php
+++ includes/utils/format_term.php
@@ -3,13 +3,17 @@
 function jam_cms_format_term($term){
   $term = (object) $term;
 
+  $uri = get_term_link($term->term_id);
+  $uri = jam_cms_format_url($uri);
+
   $formatted_term = (object) [
     'id'          => $term->term_id,
     'title'       => $term->name,
     'description' => $term->description,
     'slug'        => $term->slug,
     'parentID'    => $term->parent,
-    'count'       => $term->count
+    'count'       => $term->count,
+    'uri'         => $uri
   ];
 
   return $formatted_term;
