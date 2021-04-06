8d533825ad822e464f5855cf49591cf3de573fad
Robin Zimmer <robinzimmer1989@web.de>
2021-04-05 17:04:38 -0700

    adjust for new template naming logic

--- includes/utils/get_template_key.php
+++ includes/utils/get_template_key.php
@@ -1,17 +1,22 @@
 <?php
 
-function jam_cms_get_template_key($post_id){
+function jam_cms_get_template_key($post_id, $prefix = true){
 
   $post_type = get_post_type($post_id);
 
   // Get template file. WP returns empty string default template is active.
   $template = get_page_template_slug($post_id);
   
-  if(!$template){
+  if($template){
+    // Manually created templates are stored in the format 'template-name.php'.
+    // So we need to extract the actual title here.
+    $template = str_replace('template-', '', $template);
+    $template = str_replace('.php', '', $template);
+  }else{
     $template = "default";
   }
 
-  return "{$post_type}-{$template}";
+  return $prefix ? "{$post_type}-{$template}" : $template;
 }
 
 ?>
