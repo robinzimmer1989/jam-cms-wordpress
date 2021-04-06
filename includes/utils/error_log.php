fafd370114e648dc1037dd461b14af1f6a870e61
Robin Zimmer <robinzimmer1989@web.de>
2021-04-05 17:03:36 -0700

    Create error_log.php

---
+++ includes/utils/error_log.php
@@ -0,0 +1,15 @@
+<?php
+
+if (!function_exists('l')) {
+    function l($log) {
+        if (true === WP_DEBUG) {
+            if (is_array($log) || is_object($log)) {
+                error_log(print_r($log, true));
+            } else {
+                error_log($log);
+            }
+        }
+    }
+}
+
+?>
