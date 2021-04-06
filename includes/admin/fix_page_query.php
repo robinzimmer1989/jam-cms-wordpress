62312a95f24103f54c631b6bb0e26e782b1aff8f
Robin Zimmer <robinzimmer1989@web.de>
2021-04-05 17:01:09 -0700

    clean up

--- includes/admin/fix_page_query.php
+++ includes/admin/fix_page_query.php
@@ -2,12 +2,12 @@
 
 // Set pages to be publicly queryable. This way we can filter out 'real' post types later.
 
+add_action( 'init', 'jam_cms_fix_page_query', 1 );
 function jam_cms_fix_page_query() {
   if ( post_type_exists( 'page' ) ) {
       global $wp_post_types;
       $wp_post_types['page']->publicly_queryable = true;
   }
 }
-add_action( 'init', 'jam_cms_fix_page_query', 1 );
 
 ?>
