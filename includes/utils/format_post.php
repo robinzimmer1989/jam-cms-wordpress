c80b8cbb2dd8cdbb9f20653127143916f5ee3acd
Robin Zimmer <robinzimmer1989@web.de>
2021-04-05 17:04:00 -0700

    taxonomy and template tweaks

--- includes/utils/format_post.php
+++ includes/utils/format_post.php
@@ -1,6 +1,6 @@
 <?php
 
-function jam_cms_format_post($post, $mode = 'dev') {
+function jam_cms_format_post($post) {
 
   $thumbnail_id = get_post_thumbnail_id($post->ID);
   $formatted_media_item = null;
@@ -10,36 +10,34 @@ function jam_cms_format_post($post, $mode = 'dev') {
     $formatted_media_item = jam_cms_format_acf_field_value_for_frontend(['type' => 'image'], $media_item);
   }
 
-  if($mode == 'dev'){
-    $slug = $post->post_name;
-  }else{
-    $slug = jam_cms_generate_slug_by_id($post->ID);
-  }
+  $slug = $post->post_name;
   
   // Remove "_trashed" affix added by WordPress
   if($post->post_status == 'trash'){
     $slug = str_replace('__trashed', '', $slug);
   }
 
-  // Get template file. WP return empty string if default template.
-  $template = get_page_template_slug($post->ID);
-  if(!$template){
-    $template = "default";
-  }
+  // Get template
+  $template = jam_cms_get_template_key($post->ID, false);
 
   // Get taxonomies
   $taxonomies = get_object_taxonomies($post->post_type);
   
   $formatted_taxonomies = [];
   foreach($taxonomies as $taxonomy_name){
-    $terms = wp_get_post_terms($post->ID, $taxonomy_name);
 
-    $term_ids = [];
-    foreach($terms as $term){
-      array_push($term_ids, $term->term_id);
+    $taxonomy = get_taxonomy($taxonomy_name);
+
+    if ($taxonomy->publicly_queryable && $taxonomy->name != 'post_format') {
+      $terms = wp_get_post_terms($post->ID, $taxonomy_name);
+
+      $term_ids = [];
+      foreach($terms as $term){
+        array_push($term_ids, $term->term_id);
+      }
+
+      $formatted_taxonomies[$taxonomy_name] = $term_ids;
     }
-
-    $formatted_taxonomies[$taxonomy_name] = $term_ids;
   }
 
   $formatted_post = [
