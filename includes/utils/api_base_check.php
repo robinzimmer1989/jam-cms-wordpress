d5398f989dd15d5e5e03f69df08c4498fa5beba7
Robin Zimmer <robinzimmer1989@web.de>
2021-05-08 20:39:55 -0700

    add api key check

--- includes/utils/api_base_check.php
+++ includes/utils/api_base_check.php
@@ -2,6 +2,23 @@
 
 function jam_cms_api_base_check($parameters, $required_args = []){
 
+  if(array_key_exists('apiKey', $required_args)){
+    
+    $api_key = $parameters['apiKey'];
+
+    $api_key_db = get_option('deployment_api_key');
+
+    if(
+        !isset($api_key_db) ||
+        !$api_key_db ||
+        !isset($api_key) ||
+        !$api_key || 
+        $api_key != $api_key_db
+    ){
+        return new WP_Error( 'rest_incorrect_api_key', __( 'Api key incorrect.' ), array( 'status' => 403 ));
+    }
+  }
+
   if(is_multisite()){
 
     if(!array_key_exists('siteID', $parameters)) {
