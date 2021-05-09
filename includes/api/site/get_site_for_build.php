d9283286a052e81f6096c64364ddd9e1409d7afa
Robin Zimmer <robinzimmer1989@web.de>
2021-05-08 20:41:03 -0700

    remove api key check (part of base check now)

--- includes/api/site/get_site_for_build.php
+++ includes/api/site/get_site_for_build.php
@@ -13,21 +13,8 @@ function jam_cms_api_get_site_for_build_callback($data) {
   
     jam_cms_api_base_check($parameters, ['apiKey']);
 
-    $site_id = array_key_exists('siteID', $parameters) ? $parameters['siteID'] : 'default';
-    $api_key = $parameters['apiKey'];
-
-    $api_key_db = get_option('deployment_api_key');
-
-    if(
-        !isset($api_key_db) ||
-        !$api_key_db ||
-        !isset($api_key) ||
-        !$api_key || 
-        $api_key != $api_key_db
-    ){
-        return new WP_Error( 'rest_incorrect_api_key', __( 'Api key incorrect.' ), array( 'status' => 403 ));
-    }
-
+    $site_id = array_key_exists('siteID', $parameters) ? $parameters['siteID'] : 'default';  
+    
     $data = jam_cms_get_site_for_build_by_id($site_id);
     
     return $data;
