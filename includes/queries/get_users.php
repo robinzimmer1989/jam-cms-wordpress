9c8f960f9c49734135ce6d8870d9e987f0718ea9
Robin Zimmer <robinzimmer1989@web.de>
2021-05-30 12:30:07 -0700

    fix: don't return current user

--- includes/queries/get_users.php
+++ includes/queries/get_users.php
@@ -7,10 +7,12 @@ function jam_cms_get_users($limit = 10, $page = 0){
     'offset' => $page * $limit
   ]);
 
+  $current_user_id = get_current_user_id();
+
   $formatted_users = [];
   foreach($users as $user){
-    // Don't return super admin
-    if($user->ID != 1){
+    // Don't return super admin and current user profile
+    if($user->ID != 1 && $user->ID != $current_user_id){
       array_push($formatted_users, jam_cms_format_user($user));
     }
   }
