dac7ba50e1272398823bd07592e746edbf220745
Robin Zimmer <robinzimmer1989@web.de>
2021-06-03 22:02:24 -0700

    prettify setting updates

--- includes/utils/format_action_monitor.php
+++ includes/utils/format_action_monitor.php
@@ -4,12 +4,22 @@ function jam_cms_format_action_monitor($post){
 
   $formatted_content = json_decode($post->post_content, true);
 
-  if($formatted_content['action_type'] == 'DIFF_SCHEMAS'){
-
-    // DIFF_SCHEMAS isn't very editor friendly, so we mnually gonna change the type here
+  // Some action types aren't very user friendly,
+  // so we mnually gonna change the attributes here if necessary
+  if($formatted_content['action_type'] == 'DIFF_SCHEMAS'){  
+    
     $formatted_post = [
       'id'              => $post->ID,
       'title'           => 'Theme Settings',
+      'description'     => 'options',
+      'actionType'      => 'UPDATE'
+    ];
+
+  }elseif($formatted_content['action_type'] == 'NON_NODE_ROOT_FIELDS'){
+
+    $formatted_post = [
+      'id'              => $post->ID,
+      'title'           => $post->post_title,
       'description'     => 'options',
       'actionType'      => 'UPDATE'
     ];
