b213023f2dc8d65159eb131cbb988d2eefc10978
Robin Zimmer <robinzimmer1989@web.de>
2021-07-02 18:59:18 -0700

    version 1.8.4

--- jam-cms.php
+++ jam-cms.php
@@ -8,7 +8,7 @@
  * Plugin Name:       jamCMS
  * Plugin URI:        https://github.com/robinzimmer1989/jam-cms-wordpress
  * Description:       A CMS for the JAMStack world. Made for developers.
- * Version:           1.8.3
+ * Version:           1.8.4
  * Author:            Robin Zimmer
  * Author URI:        https://github.com/robinzimmer1989
  * License:           
