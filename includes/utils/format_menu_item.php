ddaf395093cd1f5e4aa93b2343a87b043f9b7c02
Robin Zimmer <robinzimmer1989@web.de>
2021-07-10 12:40:43 -0700

    fix menu item formatting for private posts

--- includes/utils/format_menu_item.php
+++ includes/utils/format_menu_item.php
@@ -2,12 +2,28 @@
 
 function jam_cms_format_menu_item($menu_item){
 
+  if($menu_item->object == 'custom'){
+    $post_id = null;
+    $post_type_id = null;
+    $url = jam_cms_format_url($menu_item->url);
+
+  }else {
+    $post_id = $menu_item->object_id;
+    $post_type_id = $menu_item->object;
+
+    $post = get_post($post_id);
+    // Private posts are returning encrypted urls and setting the filter to 'sample' fixes that (see https://developer.wordpress.org/reference/functions/get_sample_permalink/)
+    $post->filter = 'sample';
+    $permalink = get_permalink( $post );
+    $url = jam_cms_format_url($permalink);
+  }
+
   $new_menu_item = (object) [
     'key'         => $menu_item->ID,
-    'postID'      => $menu_item->object == 'custom' ? null : (int) $menu_item->object_id,
-    'postTypeID'  => $menu_item->object == 'custom' ? null : $menu_item->object,
+    'postID'      => $post_id,
+    'postTypeID'  => $post_type_id,
     'title'       => $menu_item->title,
-    'url'         => jam_cms_format_url($menu_item->url),
+    'url'         => $url,
     'children'    => $menu_item->children ? $menu_item->children : [],
   ];
 
