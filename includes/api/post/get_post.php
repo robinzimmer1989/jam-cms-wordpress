b58011a86f7313cb4517fc367d1e50be975fde03
Robin Zimmer <robinzimmer1989@web.de>
2021-07-10 12:41:29 -0700

    allow subscribers to fetch posts

--- includes/api/post/get_post.php
+++ includes/api/post/get_post.php
@@ -6,7 +6,7 @@ function jam_cms_api_get_post() {
         'methods' => 'POST',
         'callback' => 'jam_cms_api_get_post_callback',
         'permission_callback' => function () {
-            return current_user_can( 'edit_posts' );
+            return current_user_can('read');
         }
     ));
 }
@@ -22,12 +22,14 @@ function jam_cms_api_get_post_callback($data) {
 
     $post_id = $parameters['postID'];
 
-    // Check if post is locked
-    $is_locked = jam_cms_check_post_lock($post_id);
+    if(current_user_can('edit_posts')){
+        // Check if post is locked
+        $is_locked = jam_cms_check_post_lock($post_id);
 
-    if(!$is_locked){
-        // Lock post so other users can't edit it at the same time
-        jam_cms_set_post_lock($post_id);
+        if(!$is_locked){
+            // Lock post so other users can't edit it at the same time
+            jam_cms_set_post_lock($post_id);
+        }
     }
 
     $post = jam_cms_get_post_by_id($post_id);
