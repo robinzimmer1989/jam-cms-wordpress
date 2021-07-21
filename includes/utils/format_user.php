ba20b6d3dbea4815ef8f9237bb678e9a4c8615ef
Robin Zimmer <robinzimmer1989@web.de>
2021-07-21 13:22:56 -0700

    return roles array

--- includes/utils/format_user.php
+++ includes/utils/format_user.php
@@ -7,7 +7,7 @@ function jam_cms_format_user($user) {
   $formatted_user['id']             = $user->ID;
   $formatted_user['email']          = $user->data->user_email;
   $formatted_user['capabilities']   = $user->allcaps;
-  $formatted_user['role']           = count($user->roles) > 0 ? $user->roles[0] : '';
+  $formatted_user['roles']           = $user->roles;
 
   return $formatted_user;
   
